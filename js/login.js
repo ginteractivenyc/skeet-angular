58d120ceaa4d47f044bec5605f3172e5175849ee
gregdsign <gregatria@gmail.com>
2015-04-07 00:39:44 -0400

    soundcloud

--- js/login.js
+++ js/login.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 Parse.initialize("tzlVexuKShRsUHAGSV30qJYz28953tIOPSs0dl3z", "F1g9SlNa2FhcneKqj4AdowudvI7zkMXNZUsjtgJm");
-
+// initialize client with app credentials
+SC.initialize({
+  client_id: '07b0e9b7e4ac9e8454b61d33eaba766b',
+  redirect_uri: 'http://localhost:8888/skeet-angular/loginfiles/callback.html'
+});
 });
 
 var loginSubmit = document.getElementById('loginSubmit');
@@ -17,6 +21,9 @@ user.set("email", userEmail);
 user.signUp(null, {
   success: function(user) {
    // alert()// Hooray! Let them use the app now.
+   $('#skeetLogin').fadeOut();
+// initiate auth popup
+
     return false;
   },
   error: function(user, error) {
@@ -25,4 +32,14 @@ user.signUp(null, {
   }
 });
 
+});
+
+$('#connectSoundCloud').click(function(){
+SC.connect(function(){
+      SC.get("/me", function(me){
+        $("#username").text(me.username);
+         console.log(me.username);
+        $("#description").val(me.description);
+      });
+    });
 });
