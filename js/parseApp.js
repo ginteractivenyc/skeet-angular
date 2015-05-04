6074d8dd55efe861ef8fa6396a8a6886c42ae263
Atria <gregatria@gmail.com>
2015-05-04 18:26:06 -0400

    new

--- js/parseApp.js
+++ js/parseApp.js
@@ -1,5 +1,11 @@
 Parse.initialize("tzlVexuKShRsUHAGSV30qJYz28953tIOPSs0dl3z", "F1g9SlNa2FhcneKqj4AdowudvI7zkMXNZUsjtgJm");
 
+SC.initialize({
+  client_id: '07b0e9b7e4ac9e8454b61d33eaba766b',
+  redirect_uri: 'http://localhost:8888/skeet-angular/loginfiles/callback.html'
+});
+
+
 var nameHolderMain = [];
 var skeetApp = angular.module('skeetApp',  [ 'angular-carousel','ngRoute', 'ngResource', 'parseService']);
 
@@ -23,7 +29,7 @@ skeetApp.config(['$routeProvider', function($routeProvider) {
     });
 }]);
 
-skeetApp.controller('signupCtrl', function($scope, $location){
+skeetApp.controller('signupCtrl', function($scope, $window){
 
 var user = new Parse.User();
 var loginSubmit = document.getElementById('loginSubmit');
@@ -36,12 +42,11 @@ var userEmail= document.getElementById('inputEmail').value;
 user.set("username", userName);
 user.set("password", userPass);
 user.set("email", userEmail);
- console.log(userName + userEmail)
 user.signUp(null, {
-  success: function(user) {
+  success: function(user, $location) {
    // alert()// Hooray! Let them use the app now.
    $('#skeetLogin').fadeOut();
-   
+ $window.location='#/loggedin';
 // initiate auth popup
 
     return false;
@@ -157,6 +162,24 @@ $scope.addMusic = function(){
 $scope.addVideo = function(){
   $('#addVideoBox').fadeIn();
 }
+
+$scope.musicLogin = function(){
+SC.connect(function(){
+      SC.get("/me", function(me){
+        var soundcloudName = me.username;
+        $("#username").text(soundcloudName);
+         console.log(soundcloudName);
+        $("#description").val(me.description);
+        var currentUser = Parse.User.current();
+
+        //save soundcloud Name to parse
+        currentUser.set("soundcloud",soundcloudName );
+        currentUser.save();
+
+      });
+    });
+}
+
 
 });
 skeetApp.filter('artworkCheck', function () {
